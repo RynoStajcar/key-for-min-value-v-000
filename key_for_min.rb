80195a4bec0a2e3948c51691d89c5fcb2f10496e
Ryne Stajcar <rynostajcar@gmail.com>
2018-01-10 22:06:34 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,9 +3,10 @@
 require 'pry'
 def key_for_min_value(name_hash)
   low_value = ""
-  binding.pry
+
   name_hash.each do |name, value|
    if value.length < low_value[0].length
+     binding.pry
      low_value << value
    end
  end
